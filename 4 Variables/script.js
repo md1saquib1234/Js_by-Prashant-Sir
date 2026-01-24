b29e23ab4f8bfaef88b15aeab5ddb9a839badb57
MD SAQUIB ANSARI <mdshaanansari0786@gmail.com>
2026-01-24 22:53:39 +0530

    Add various HTML, CSS, and JavaScript files for functions, objects, and games

---
+++ 4 Variables/script.js
@@ -0,0 +1,25 @@
+// let welcomeMessage = 'welcome';
+// console.log(welcomeMessage);
+// console.log('welcome');
+// let bagPrice = (15599 - (10/100 * 15599)) - 272;
+// let jeansPrice = (2499 - (41/100 * 2499)) - 29;
+// let bagValue = Math.round((jeansPrice + bagPrice  + 25 +20) * 118 / 100);
+// let bagSummary = `Your final Myntra Bag amount is ₹${bagValue}`;
+// console.log(bagSummary);
+let a = 5;
+let b = 6;
+let c = a+4;
+console.log(c);
+c = b+2;
+console.log(c);
+
+let i = 5;
+console.log(i);
+i += 5;
+console.log(i);
+
+const pi = 3.14159265359;
+let r = 4;
+// pi = 2;
+let area = pi * r**2;
+console.log(area);
---
+++ scripts/index.js
@@ -0,0 +1,2 @@
+console.log('I am coming from index.js file');
+console.clear();
